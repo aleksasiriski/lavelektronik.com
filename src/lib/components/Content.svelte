284eeba35281423644816667dd1056d2f086bdaa
Aleksa Siriški <31509435+aleksasiriski@users.noreply.github.com>
2023-11-25 20:14:36 +0100

    tailwindcss

---
+++ src/lib/components/Content.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+	import logo from '$lib/assets/logo_blue.svg';
+</script>
+
+<div class="w-full">
+	<div class="mx-auto max-w-lg text-center">
+		<img class="mx-auto h-auto w-100" src={logo} alt="SZR Lav Elektronik Logo" />
+		<h1 class="mt-6 text-5xl font-bold">Lav Elektronik</h1>
+		<hr class="mt-6" />
+		<p class="feature-p">Arhitektura mreže i sprovođenje kablova.</p>
+		<p class="feature-p">Video nadzorni sistemi sa online pristupom preko mobilnog.</p>
+		<p class="feature-p">Senzori na pokret, zaključavanje vrata i prozora.</p>
+		<p class="feature-p">Kontrola pristupa i radnog vremena sa čipovima i karticama.</p>
+		<p class="feature-p">Klizne i motorne kapije kontrolisane sa daljinskim i mobilnim.</p>
+		<hr class="mt-6" />
+		<h2 class="mt-6 text-3xl font-light">Kontakt</h2>
+		<p class="contact-p">Slavko Siriški</p>
+		<p class="contact-p">
+			<a aria-label="Contact phone number" href="tel:+38163505192">063-505-192</a>
+		</p>
+	</div>
+</div>
