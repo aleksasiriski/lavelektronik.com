0a38836f490ab4262de8e1ce41dce4796a67b4ea
Aleksa Siriški <31509435+aleksasiriski@users.noreply.github.com>
2023-12-04 22:05:37 +0100

    fix: remove fallback

--- svelte.config.js
+++ svelte.config.js
@@ -14,9 +14,6 @@ const config = {
 		adapter: adapter({
 			// default options are shown. On some platforms
 			// these options are set automatically — see below
-			pages: 'build',
-			assets: 'build',
-			fallback: 'index.html',
 			precompress: true,
 			strict: true
 		})
